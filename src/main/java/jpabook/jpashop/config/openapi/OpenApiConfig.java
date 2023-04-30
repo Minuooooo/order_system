1ba2b3a01d2cab132fb71f854978419cc42b89bf
Minuooooo <mmw7741@gmail.com>
2023-05-01 01:08:16 +0900

    [ feat ] OpenAPI Swagger 작용

---
+++ src/main/java/jpabook/jpashop/config/openapi/OpenApiConfig.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.config.openapi;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+
+        final String securitySchemeName = "bearerAuth";
+
+        Info info = new Info()
+                .title("Order API")
+                .version("v1.0.0")
+                .description("for order application frontend");
+
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .info(info);
+    }
+}
---
+++ src/main/java/jpabook/jpashop/config/security/SecurityConfig.java
@@ -0,0 +1,51 @@
+package jpabook.jpashop.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private static final String[] AUTH_WHITELIST = {
+            "/swagger-ui/**",
+            "/api-docs/**",
+            "/api"
+    };
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    protected SecurityFilterChain config(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+//                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+//                .exceptionHandling()
+//                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+//                .accessDeniedHandler(jwtAccessDeniedHandler)
+//
+//                .and()
+//                .apply(new JwtSecurityConfig(jwtProvider))
+//
+//                .and()
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//
+//                .and()
+                .authorizeHttpRequests(authorize -> authorize
+                        .shouldFilterAllDispatcherTypes(false)
+                        .antMatchers(AUTH_WHITELIST)
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .build();
+    }
+}
